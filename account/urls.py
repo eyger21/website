94c9c4e1736f4786ab70a0deb9d3a65d42f6ce11
eyger <eyger21@gmail.com>
2021-02-04 14:45:30 +0100

    social website

---
+++ account/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+
+    #previous login
+    #path('login/', views.user_login, name='login'),
+
+    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
